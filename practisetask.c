78d903620d1ea92ff48f8d2fe11b67ebe4d637fd
bhoomika-rohra <K250784@nu.edu.pk>
2025-11-12 00:33:21 +0500

    Update practisetask.c

--- practisetask.c
+++ practisetask.c
@@ -8,7 +8,7 @@ int main() {
     float kelvin = celsius + 273.15f;
     float fahrenheit = celsius * 1.80f + 32.00f;
 
-    printf("Resulting array: [%.5f, %.5f]\n", kelvin, fahrenheit);
+    printf("[%.5f, %.5f]\n", kelvin, fahrenheit);
 
     return 0;
 }
